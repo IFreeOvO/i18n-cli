476fcd1553a304d910cf2e48612fc3abb29929b6
ifreeOvO <mryq2020@163.com>
2022-11-29 00:31:56 +0800

    ci: 添加commitlint校验规则

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,26 @@
 
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'release',
+      ],
+    ],
+  },
   prompt: {
     types: [
       { value: 'feat', name: 'feat:     A new feature', emoji: ':sparkles:' },
