aaae11bbd36ac3ccb8bee3bcee26d41ab70ee71f
ifreeOvO <mryq2020@163.com>
2022-12-04 19:07:07 +0800

    fix: vue的props属性,转换效果不符合预期

---
+++ .changeset/fluffy-geese-exercise.md
@@ -0,0 +1,5 @@
+---
+'@ifreeovo/i18n-extract-cli': patch
+---
+
+修复 vue 的 props 属性,转换效果不符合预期
--- packages/i18n-extract-cli/src/transformVue.ts
+++ packages/i18n-extract-cli/src/transformVue.ts
@@ -153,6 +153,7 @@ function handleTemplate(code: string, rule: Rule): string {
 function handleScript(source: string, rule: Rule): string {
   const { code } = transformJs(source, 'tsx', {
     rule,
+    isJsInVue: true, // 标记处理vue里的js
     parse: initParse([[presetTypescript, { isTSX: true, allExtensions: true }]]),
   })
   return code
