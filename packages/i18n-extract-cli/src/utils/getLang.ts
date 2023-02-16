a5e13da72e06fe2f1389e04a45c0439cac2adc27
ifreeOvO <mryq2020@163.com>
2023-02-17 00:13:37 +0800

    fix: 中文语言包存在的情况下进行翻译，其他语言包不会新增key-value

--- packages/i18n-extract-cli/src/utils/getLang.ts
+++ packages/i18n-extract-cli/src/utils/getLang.ts
@@ -1,6 +1,9 @@
+import fs from 'fs-extra'
+
 function getLang(langPath: string): Record<string, string> {
   try {
-    return require(langPath)
+    const content = fs.readFileSync(langPath).toString()
+    return JSON.parse(content)
   } catch {
     return {}
   }
