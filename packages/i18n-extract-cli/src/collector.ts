47f478f1b7703a5ecb1a60b260b1ed9ac0771010
ifreeOvO <mryq2020@163.com>
2023-03-11 12:57:39 +0800

    refactor: 修改单例模式设计

--- packages/i18n-extract-cli/src/collector.ts
+++ packages/i18n-extract-cli/src/collector.ts
@@ -1,15 +1,31 @@
 import log from './utils/log'
 
 class Collector {
-  static keyMap: Record<string, string> = {}
-
+  private static _instance: Collector
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   private constructor() {}
 
-  static add(key: string) {
+  static getInstance() {
+    if (!this._instance) {
+      this._instance = new Collector()
+    }
+    return this._instance
+  }
+
+  private keyMap: Record<string, string> = {}
+
+  add(key: string) {
     log.verbose('提取中文：', key)
-    Collector.keyMap[key] = key
+    this.keyMap[key] = key
+  }
+
+  getKeyMap() {
+    return this.keyMap
+  }
+
+  setKeyMap(value: Record<string, string>) {
+    this.keyMap = value
   }
 }
 
-export default Collector
+export default Collector.getInstance()
