4516e6d0a6bd64370a1d99dc3c14442520126023
ifreeOvO <mryq2020@163.com>
2023-04-23 01:31:30 +0800

    refactor: 修改js类型语言包中的导出语句
    #72

--- packages/i18n-extract-cli/src/utils/serializeCode.ts
+++ packages/i18n-extract-cli/src/utils/serializeCode.ts
@@ -3,9 +3,9 @@ import serialize from 'serialize-javascript'
 
 export function serializeCode(source: unknown) {
   const code = `
-    module.exports = ${serialize(source, {
-      unsafe: true,
-    })}
+  export default ${serialize(source, {
+    unsafe: true,
+  })}
   `
   const stylizedCode = prettier.format(code, {
     semi: false,
