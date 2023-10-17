825c8e009f0d294df0dc96f11bb894340ab6a376
ifreeOvO <mryq2020@163.com>
2023-10-17 23:26:06 +0800

    fix: 读取配置文件报错
    #106

--- packages/i18n-extract-cli/src/utils/serializeCode.ts
+++ packages/i18n-extract-cli/src/utils/serializeCode.ts
@@ -1,9 +1,10 @@
 import prettier from 'prettier'
 import serialize from 'serialize-javascript'
 
-export function serializeCode(source: unknown) {
+export function serializeCode(source: unknown, isESModule = true) {
+  const exportStatement = isESModule ? 'export default' : 'module.exports ='
   const code = `
-  export default ${serialize(source, {
+  ${exportStatement} ${serialize(source, {
     unsafe: true,
   })}
   `
